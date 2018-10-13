0d2310ba0512413290666f7ccd8b58600cea1c1e
Tom O'Connell <toconnel@caltech.edu>
2018-10-13 16:48:38 -0700

    Added hardcoded dict of CAS numbers for problematic Hallem / Carson entries.

--- chemutils.py
+++ chemutils.py
@@ -4,6 +4,35 @@ import re
 import pubchempy as pcp
 
 
+# Procedure for entering CAS numbers in here (follow this to maintain
+# consistency of name2cas fn):
+# 1) Find pubchem page for compound of interest.
+# 2) Get one of the names under "Chemical names"
+# 3) Run name2cas on that chemical name.
+# 4) Use the output cas number here.
+manual_name2cas = {
+    'g-hexalactone': '57129-70-1',
+    'g-octalactone': '104-50-7',
+    'g-decalactone': '1336-42-1',
+    'a -pinene': '102640-64-2',
+    'b -pinene': '127-91-3',
+    # might not be right
+    '(1S)-(+)-3-carene': '1352058-87-7',
+    'a -terpineol': '10482-56-1',
+    # trans? racemic? (what to do w/ cas number in that case?)
+    'linalool oxide': '10448-29-0',
+    'E2-hexenal': '1335-39-3',
+    '4-ethyl guaiacol': '2785-89-9',
+    'E2-hexenol': '114411-82-4',
+    'Z2-hexenol': '928-94-9',
+    'E3-hexenol': '544-12-7',
+    'Z3-hexenol': '82658-58-0',
+    'E2-hexenyl acetate': '10094-40-3',
+    'spontaneous firing rate': None,
+    'odor': None
+}
+
+
 cache = dict()
 def name2cas(name, verbose=False):
     """Returns the CAS number for the chemical with the given name.
@@ -12,11 +41,14 @@ def name2cas(name, verbose=False):
     (as sorted by Python string comparison) is returned, so the answer should at
     least be consistent.
     """
+    if name in manual_name2cas:
+        return manual_name2cas[name]
+
     if name in cache:
         if verbose:
             print('{} in cache'.format(name))
         return cache[name]
-    
+
     results = pcp.get_synonyms(name, 'name')
 
     if len(results) == 0:
